1b580da5d945b74c797bd0de583a63dcf1c1e539
91762 <91762@DippalRajput9011>
2023-02-22 00:43:34 +0530

    first commit

---
+++ assignmentfunctionrecursion/src/org/tnsif/assignmentfunctionrecursion/SumOFDigitsUsingRecursion.java
@@ -0,0 +1,10 @@
+package org.tnsif.assignmentfunctionrecursion;
+
+public class SumOFDigitsUsingRecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
